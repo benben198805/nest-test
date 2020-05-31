9ba9fbea76306f6fdc640becddc48e9b1df9e47c
benben <benben198805@126.com>
2020-05-31 22:02:44 +0800

    set share module as global module

--- src/share/share.module.ts
+++ src/share/share.module.ts
@@ -1,6 +1,7 @@
-import {Module} from '@nestjs/common';
+import {Global, Module} from '@nestjs/common';
 import {ShareService} from './share.service';
 
+@Global()
 @Module({
     providers: [ShareService],
     exports: [ShareService]
--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,12 +1,10 @@
 import {Module} from '@nestjs/common';
 import {TodoController} from "./todo.controller";
 import {TodoService} from "./todo.service";
-import {ShareModule} from "../share/share.module";
 
 @Module({
     controllers: [TodoController],
-    providers: [TodoService],
-    imports: [ShareModule]
+    providers: [TodoService]
 })
 export class TodoModule {
 }
