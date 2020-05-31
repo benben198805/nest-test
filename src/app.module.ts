030fd54b8f6823113afd36ea365060410fc11b9f
benben <benben198805@126.com>
2020-05-31 22:07:09 +0800

    add logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
-import {Module} from '@nestjs/common';
+import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {TodoModule} from './todo/todo.module';
-import { ShareModule } from './share/share.module';
+import {ShareModule} from './share/share.module';
+import {LoggerMiddleware} from "./logger.middleware";
+import {TodoController} from "./todo/todo.controller";
 
 @Module({
     imports: [TodoModule, ShareModule],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer): any {
+        consumer.apply(LoggerMiddleware)
+            .forRoutes(TodoController);
+    }
 }
