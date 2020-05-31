84ec779d17392e5f5de0db1ca5b637a1ec1357a5
benben <benben198805@126.com>
2020-05-31 23:01:28 +0800

    use nestjs inner validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
+import {MiddlewareConsumer, Module, NestModule, ValidationPipe} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {TodoModule} from './todo/todo.module';
@@ -6,14 +6,13 @@ import {ShareModule} from './share/share.module';
 import {LoggerMiddleware} from "./logger.middleware";
 import {TodoController} from "./todo/todo.controller";
 import {APP_PIPE} from "@nestjs/core";
-import {ValidatePipe} from "./validate.pipe";
 
 @Module({
     imports: [TodoModule, ShareModule],
     controllers: [AppController],
     providers: [AppService, {
         provide: APP_PIPE,
-        useClass: ValidatePipe
+        useClass: ValidationPipe
     }],
 })
 export class AppModule implements NestModule {
