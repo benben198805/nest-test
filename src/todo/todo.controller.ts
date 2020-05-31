583e46b1383e7b20563a6363ea119888a7e3e9a6
benben <benben198805@126.com>
2020-05-31 21:50:41 +0800

    add todo service logic

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,25 +1,30 @@
 import {Body, Controller, Delete, Get, HttpCode, Param, Post, Put} from '@nestjs/common';
 import {ToDoItem} from "./doto-item.dto";
+import {TodoService} from "./todo.service";
 
 @Controller('todo')
 export class TodoController {
 
+    constructor(private toDoService: TodoService) {
+    }
+
     @Get()
     findAll(): ToDoItem[] {
-        return [new ToDoItem()];
+        return this.toDoService.findAll();
     }
 
 
     @Get(':id')
     find(@Param() params): ToDoItem {
         console.log(params.id);
-        return new ToDoItem();
+        return this.toDoService.find(params.id);
     }
 
     @Post()
     @HttpCode(201)
     create(@Body() createToDoItem: ToDoItem) {
         console.log(createToDoItem);
+        this.toDoService.create(createToDoItem);
         return "created";
     }
 
@@ -27,12 +32,14 @@ export class TodoController {
     update(@Param('id') id: string, @Body() updateToDoItem: ToDoItem) {
         console.log(id);
         console.log(updateToDoItem);
+        this.toDoService.update(id, updateToDoItem);
         return "updated";
     }
 
     @Delete(':id')
     delete(@Param('id') id: string) {
         console.log(id);
+        this.toDoService.delete(id);
         return "deleted";
     }
 }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {ToDoItem} from "./doto-item.dto";
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+    private todos: ToDoItem[] = [];
+
+    find(id: string) {
+        return this.todos.find(it => it.id === id);
+    }
+
+    findAll() {
+        return this.todos;
+    }
+
+    create(item: ToDoItem) {
+        this.todos.push(item);
+    }
+
+    update(id, item: ToDoItem) {
+        let index = this.todos.findIndex(item => item.id === id);
+        if (index > -1) {
+            this.todos[index] = {
+                ...item,
+                id
+            };
+        }
+    }
+
+    delete(id) {
+        this.todos.filter(item => item.id === id);
+    }
+
+}
