34357e73b5397434b0fc5e9c0f7fd099e68cbc3c
benben <benben198805@126.com>
2020-05-31 21:55:06 +0800

    add todo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TodoController } from './todo/todo.controller';
-import { TodoService } from './todo/todo.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {TodoModule} from './todo/todo.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, TodoController],
-  providers: [AppService, TodoService],
+    imports: [TodoModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
