b1317bc2d68f00b43d911346a1dbc149d76f622e
benben <benben198805@126.com>
2020-06-02 14:49:08 +0800

    add helmet and csurf

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,24 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import * as compression from 'compression';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
 
 declare const module: any;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.use(helmet());
+
     if (module.hot) {
         module.hot.accept();
         module.hot.dispose(() => app.close());
     }
+
+    app.enableCors();
+
+    app.use(csurf());
 
     app.use(compression());
 
